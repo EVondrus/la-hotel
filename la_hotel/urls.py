e3cda42888c81fa52caf9b85981e13db59209b22
Ellenor Vondrus <ellavondrus@gmail.com>
2024-06-09 15:59:52 +0200

    update: URL for home page

--- la_hotel/urls.py
+++ la_hotel/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from home import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('home/', index_views.index, name='home'),
     path('admin/', admin.site.urls),
 ]
