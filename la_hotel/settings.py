19a3a3f5781cfcf771e7184294044b21ab1e5e9e
Ellenor Vondrus <ellavondrus@gmail.com>
2024-06-10 09:59:23 +0200

    refactor: Update logging configuration to handle errors in Django

--- la_hotel/settings.py
+++ la_hotel/settings.py
@@ -149,3 +149,23 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
