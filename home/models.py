6c829603fe3ce00421df87d585aa764faebfa22e
Ellenor Vondrus <ellavondrus@gmail.com>
2024-06-11 12:28:09 +0200

    feat: Add Hotel model to home app and admin registration
    Install pillow for Images

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Hotel(models.Model):
+    """
+    Define the hotel information
+    """
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images')
+    description = models.TextField()
+    avaliable_rooms = models.IntegerField()
+
+    def __str__(self):
+        return f'Hotel name: {self.name}'
+        
