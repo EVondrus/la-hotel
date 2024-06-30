647ec8c2fe901375702de732e305769d5c161bf7
Ellenor Vondrus <ellavondrus@gmail.com>
2024-06-30 18:55:18 +0200

    chore: Install Django Resized package

--- la_hotel/settings.py
+++ la_hotel/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -63,7 +64,6 @@ INSTALLED_APPS = [
     'django_summernote',
     'djmoney',
     'cloudinary',
-    'cloudinary_storage',
 
 ]
 
