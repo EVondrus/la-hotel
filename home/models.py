84d8f8957a1cb3ae217e5083702c8913bf47646b
Ellenor Vondrus <ellavondrus@gmail.com>
2024-06-30 19:10:32 +0200

    refactor: Enhance Hotel model with detailed attributes and image optimization
    Added unique constraints to name, email, and phone fields to ensure data integrity and refactor code to comply with PEP 8 standard

--- home/models.py
+++ home/models.py
@@ -1,20 +1,37 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
+
 
 class Hotel(models.Model):
     """
     Define the hotel information
     """
-    name = models.CharField(max_length=100)
-    adress = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images')
-    description = models.TextField()
-    avaliable_rooms = models.IntegerField()
+    name = models.CharField(
+        max_length=100, unique=True, blank=False, null=False)
+    adress = models.CharField(
+        max_length=100, blank=False, null=False)
+    zip_code = models.CharField(
+        max_length=20, blank=False, null=False)
+    city = models.CharField(
+        max_length=100, blank=False, null=False)
+    email = models.EmailField(
+        max_length=100, unique=True, blank=False, null=False)
+    phone = models.CharField(
+        max_length=20, unique=True, blank=False, null=False)
+    image = ResizedImageField(
+        size=[550, None], quality=75, upload_to="hotel",
+        force_format="WEBP", blank=False, null=False
+    )
+    image_alt = models.CharField(
+        max_length=100, help_text="Enter a breif description of the image",
+        default="placeholder default hotel image",
+        blank=False, null=False
+    )
+    description = models.TextField(
+        help_text="Enter a description of the hotel", blank=False, null=False)
+    avaliable_rooms = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'Hotel name: {self.name}'
