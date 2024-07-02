7ca21c6727f28728a09e278338abaf78d5494cd1
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-02 23:39:56 +0200

    feat: Update guest profile page template and add Edit Profile functionality in a modal and add url pattern

--- guests/urls.py
+++ guests/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Profiles
+from .views import Profiles, EditProfile
 
 app_name = 'guests'
 
 urlpatterns = [
     path('user/<int:pk>/', Profiles.as_view(), name='profile'),
+    path('edit/<int:pk>/', EditProfile.as_view(), name='edit_profile')
 ]
