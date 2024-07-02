f9c89cc6dddab204d178f3203f67704ff6904f13
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-02 23:38:44 +0200

    feat: Add EditProfile view for updating guest profile

--- guests/views.py
+++ guests/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.shortcuts import redirect
+
 from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -13,6 +18,25 @@ class Profiles(TemplateView):
     def get_context_data(self, **kwargs):
         profile = Profile.objects.get(user=self.kwargs['pk'])
         context = {
-            'profile': profile
+            'profile': profile,
+            'form': ProfileForm(instance=profile)
         }
+
         return context
+
+class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """ Edit Guest Profile """
+
+    model = Profile
+    form_class = ProfileForm
+
+    def form_valid(self, form):
+        form.save()  # Save the form
+        # Add a success message
+        messages.success(self.request, 'Your profile was successfully updated.')
+        # Redirect to the profile view or success_url
+        return redirect('guests:profile', pk=self.kwargs['pk'])
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+    
