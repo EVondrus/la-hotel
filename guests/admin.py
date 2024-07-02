89b9af52aeac8ad2cd895e151bbd74ee4f821d00
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-02 19:30:33 +0200

    feat: Add ProfileAdmin class to guests/admin.py

--- guests/admin.py
+++ guests/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'address',
+        'city',
+        'country',
+        'postal_code',
+        'date_of_birth',
+    )
+
+    search_fields = ('user', 'first_name', 'last_name', 'phone_number')
+
+admin.site.register(Profile, ProfileAdmin)
