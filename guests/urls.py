3cef8923f774dbbf432e6262201e8c4b14421309
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-04 23:33:18 +0200

    refactor: Add url for the customized signup form

--- guests/urls.py
+++ guests/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Profiles, EditProfile, DeleteProfile
-
+from .views import Profiles, EditProfile, DeleteProfile, CustomSignup
 
 
 urlpatterns = [
+    path('accounts/signup/', CustomSignup.as_view(), name='account_signup'),
     path('delete-profile/<int:pk>/', DeleteProfile.as_view(), name='delete_profile'),
     path('edit/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
     path('profile/<int:pk>/', Profiles.as_view(), name='profile'),
--- la_hotel/urls.py
+++ la_hotel/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
+    path('profiles/', include('guests.urls')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
     path('rooms/', include('rooms.urls'), name='rooms'),
     path('booking/', include('booking.urls'), name='booking'),
-    path('profiles/', include('guests.urls'))
+    
 ]
