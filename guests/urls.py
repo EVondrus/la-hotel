efd163ef2f926f4822f5e6604405abdc2f352012
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-04 17:15:57 +0200

    feat: Add Deleteprofile view, template and url
    deleting the guest/user account including profile and bookings and logging them out

--- guests/urls.py
+++ guests/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Profiles, EditProfile
+from .views import Profiles, EditProfile, DeleteProfile
 
-app_name = 'guests'
+
 
 urlpatterns = [
-    path('user/<int:pk>/', Profiles.as_view(), name='profile'),
-    path('edit/<int:pk>/', EditProfile.as_view(), name='edit_profile')
+    path('delete-profile/<int:pk>/', DeleteProfile.as_view(), name='delete_profile'),
+    path('edit/<int:pk>/', EditProfile.as_view(), name='edit_profile'),
+    path('profile/<int:pk>/', Profiles.as_view(), name='profile'),
 ]
--- guests/views.py
+++ guests/views.py
@@ -1,28 +1,36 @@
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView, UpdateView, DeleteView, CreateView
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.contrib.auth import logout
+
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, get_object_or_404
 
 from .models import Profile
 from .forms import ProfileForm
 
+
 # Create your views here.
 
+
 class Profiles(TemplateView):
-    """ 
-    Guest Profile view 
+    """  
     Displays guest profile information
     """
     template_name = 'guests/profiles.html'
     
     def get_context_data(self, **kwargs):
-        profile = Profile.objects.get(user=self.kwargs['pk'])
+        user_id = self.kwargs.get('pk')
+        profile = get_object_or_404(Profile, user=user_id)
         context = {
             'profile': profile,
             'form': ProfileForm(instance=profile)
         }
 
         return context
+
 
 class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """ Edit Guest Profile """
@@ -31,12 +39,40 @@ class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     form_class = ProfileForm
 
     def form_valid(self, form):
-        form.save()  # Save the form
-        # Add a success message
+        """Handle successful form submission."""
+        form.save()
         messages.success(self.request, 'Your profile was successfully updated.')
-        # Redirect to the profile view or success_url
         return redirect('guests:profile', pk=self.kwargs['pk'])
 
     def test_func(self):
+        """Ensure user can only edit their own profile."""
         return self.request.user == self.get_object().user
-    
+
+
+class DeleteProfile(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Deletes the user's profile and related bookings
+    and logs them out of the system.
+    """
+    model = Profile
+    template_name = 'allauth/account/delete_profile.html'
+    success_url = reverse_lazy('home') 
+
+    def test_func(self):
+        """Ensure user can only delete their own profile."""
+        return self.request.user == self.get_object().user
+
+
+    def form_valid(self, form):
+        """Handle successful deletion."""
+        messages.success(self.request, 'Your account has been permanently \
+        deleted, including all related bookings. Welcome back anytime!')
+
+        # Delete the user's account and related bookings
+        self.request.user.delete()
+
+        # Log out the user
+        logout(self.request)
+
+        # Redirect to the home page 
+        return HttpResponseRedirect(reverse('home'))
