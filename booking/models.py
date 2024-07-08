79dc11f02b0d6e7f6983337aa0bdfa2af26a6e68
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-08 21:24:44 +0200

    refactor: booking validation and room availability management
    Calculate total price based on room category and total nights and add get_available_rooms method to fetch available rooms within a date range
    
    Update room availability status upon booking creation and modification using signals

--- booking/models.py
+++ booking/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils import timezone
+
 from djmoney.models.fields import MoneyField
 from datetime import date
-from rooms.models import Room
+
 from django.contrib.auth.models import User
+from rooms.models import Room, RoomCategory
 
 # Create your models here.
 
@@ -30,3 +35,50 @@ class Booking(models.Model):
     class Meta:
         ordering = ['-check_in']
 
+    def save(self, *args, **kwargs):
+        # Ensure check-out date is after check-in date
+        if self.check_out <= self.check_in:
+            raise ValueError("Check-out date must be after check-in date")
+
+        # Calculate total nights
+        total_nights = (self.check_out - self.check_in).days
+
+        # All rooms in the same category have the same price
+        room_category = self.rooms.first().category
+
+        # Calculate total price
+        # price is stored in the RoomCategory model
+        price_per_night = room_category.price.amount 
+        self.total_price = price_per_night * total_nights
+
+        super(Booking, self).save(*args, **kwargs)
+
+
+    @staticmethod
+    def get_available_rooms(room_category, check_in, check_out):
+        print(f"Checking availability for: Category={room_category}, Check-in={check_in}, Check-out={check_out}")
+        available_rooms = []
+        rooms_in_category = Room.objects.filter(
+            category=room_category, avaliable=True
+        )
+
+        for room in rooms_in_category:
+            overlapping_bookings = Booking.objects.filter(
+                rooms=room,
+                check_in__lt=check_out,
+                check_out__gt=check_in
+            ).exists()
+
+            if not overlapping_bookings:
+                available_rooms.append(room)
+
+        return available_rooms
+
+
+@receiver(post_save, sender=Booking)
+def update_room_availability_on_save(sender, instance, created, **kwargs):
+    """Mark rooms as unavailable upon booking creation."""
+    if created:
+        for room in instance.rooms.all():
+            room.avaliable = False
+            room.save()
