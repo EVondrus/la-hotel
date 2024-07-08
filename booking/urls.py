8baa74802ffcb00c21a48d06cb074abe53dfb758
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-08 23:31:53 +0200

    refactor: Update sucess URL to include pk

--- booking/urls.py
+++ booking/urls.py
@@ -11,7 +11,7 @@ app_name = 'booking'
 
 urlpatterns = [
     path('', AddBooking.as_view(), name='add_booking'),
-    path('success/', BookingSuccess.as_view(), name='booking_success'),
+    path('success/<int:pk>', BookingSuccess.as_view(), name='booking_success'),
     path('list/', BookingList.as_view(), name='booking_list'),
     path('edit/<int:pk>', EditBooking.as_view(), name='edit_booking'),
     path('delete/<int:pk>/', DeleteBooking.as_view(), name='delete_booking'),
