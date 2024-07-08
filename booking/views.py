154a9e7c10ce62e385986f72020b16baa3671a76
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-08 21:40:50 +0200

    refactor: Implemented comprehensive updates to booking functionality for enhanced usability and system integrity
    Refine booking form processing in AddBooking view to streamline room assignment and error handling and enforce strict deadlines for editing and deleting bookings and correct logical errors in DeleteBooking view
    
    add BookingSuccess view to immediately confirm bookings

--- booking/views.py
+++ booking/views.py
@@ -16,8 +16,7 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
-from datetime import timedelta
-import datetime
+from datetime import timedelta, datetime
 
 from .models import Booking
 from rooms.models import Room, RoomCategory
@@ -39,6 +38,7 @@ class AddBooking(LoginRequiredMixin, CreateView):
         check_in = form.cleaned_data['check_in']
         check_out = form.cleaned_data['check_out']
 
+        # Check if the selected room is available for the selected dates
         available_rooms = Booking.get_available_rooms(
             room_category,
             check_in, check_out
@@ -49,19 +49,17 @@ class AddBooking(LoginRequiredMixin, CreateView):
                     dates. Please select another room or date.'
             )
             return self.form_invalid(form)
-        else:
-            booking = form.save(commit=False)
-            booking.user = self.request.user
-            first_available_room = available_rooms.first()
-            booking.save()
-            booking.rooms.add(first_available_room)
-
-            self.request.session['booking_id'] = booking.id
-            messages.success(
-                self.request, 'Your reservation has been confirmed. \
-                    Thank you for booking with us!'
-            )
-            return super().form_valid(form)
+        
+        # Save the booking
+        booking = form.save()
+        booking.rooms.add(available_rooms.first())  # adding the first available room
+        self.request.session['booking_id'] = booking.id
+        messages.success(
+            self.request, 'Your reservation has been confirmed. \
+                Thank you for booking with us!'
+        )
+            
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         """Handle invalid form submissions"""
@@ -72,31 +70,17 @@ class AddBooking(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
 
-# class BookingSuccess(TemplateView):
-#     template_name = 'booking/booking_success.html'
+class BookingSuccess(TemplateView):
+    """ View for displaying a success message after booking """
+    template_name = 'booking/booking_success.html'
 
-#     def get(self, request, *args, **kwargs):
-#         booking_id = request.session.get('booking_id')
-#         if booking_id:
-#             # booking_id is cleared from the session after retrieving it
-#             del request.session['booking_id']
-#             return super().get(request, *args, **kwargs)
-#         else:
-#             messages.error(request, 'Booking details not found. Please try again or contact support.')
-#             return redirect('home')
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         booking_id = self.request.session.get('booking_id', None)
-#         if booking_id:
-#             context['booking'] = get_object_or_404(Booking, id=booking_id)
-#         return context
 
 class BookingList(LoginRequiredMixin, ListView):
     """ View for listing all bookings made by the user. """
     template_name = 'booking/booking_list.html'
     context_object_name = 'bookings'
     model = Booking
+
 
 class EditBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """ View for editing a booking """
@@ -111,8 +95,10 @@ class EditBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         check_in = form.cleaned_data['check_in']
         check_out = form.cleaned_data['check_out']
 
+        # check_in is a datetime.date object from form.cleaned_data
+        check_in_datetime = timezone.make_aware(datetime.combine(check_in, datetime.min.time()))
         # Calculate the deadline for editing (15:00 on the day before check-in)
-        check_in_deadline = check_in.replace(
+        check_in_deadline = check_in_datetime.replace(
             hour=15, minute=0, second=0) - timedelta(days=1)
 
         # Check if the current time is beyond the edit deadline
@@ -133,20 +119,30 @@ class EditBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
                 self.request, 'This room is not available for the selected \
                     dates. Please select another room or date.')
             return self.form_invalid(form)
-        else:
-            booking = form.save(commit=False)
-            old_room = booking.rooms.first()  # Get the previously booked room
-            first_available_room = available_rooms.first()
-            booking.rooms.clear()  # Clear current rooms
-            booking.rooms.add(first_available_room)  # Add new available room
-            booking.save()  # Save the booking with the new room
+        
+        # Update booking details and room assignment
+        booking = form.save(commit=False)
+        old_room = booking.rooms.first() if booking.rooms.exists() else None  # Get the previously booked room, if any
 
-            # logic to make the old room available for others
-            if old_room:
-                old_room.booking_set.remove(booking)  # Remove booking from the old room
+        #adding the first available room
+        first_available_room = available_rooms.first()
+        booking.rooms.clear()  # Clear current rooms
+        booking.rooms.add(first_available_room)  # Add new available room
 
-            messages.success(self.request, 'Your booking has been updated.')
-            return super().form_valid(form)
+        # Recalculate total price based on updated booking details
+        total_nights = (check_out - check_in).days
+        price_per_night = room_category.price.amount  # Adjust if necessary
+        total_price = price_per_night * total_nights
+        booking.total_price = total_price
+        booking.save()  # Save the booking with the new room and total price
+
+        # Make the old room available for others
+        if old_room:
+            old_room.avaliable = True
+            old_room.save()
+
+        messages.success(self.request, 'Your booking has been updated.')
+        return super().form_valid(form)
 
     def test_func(self):
         """ Check if the user is the owner of the booking """
@@ -172,16 +168,18 @@ class DeleteBooking(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def delete(self, request, *args, **kwargs):
         """ Perform delete operation """
         booking = self.get_object()
+        success_url = 'home'
 
-        # Calculate the deadline for deletion (15:00 on the day before check-in)
-        check_in_deadline = booking.check_in.replace(
+        check_in_datetime = timezone.make_aware(datetime.combine(check_in, datetime.min.time()))
+        # Calculate the deadline for editing (15:00 on the day before check-in)
+        check_in_deadline = check_in_datetime.replace(
             hour=15, minute=0, second=0) - timedelta(days=1)
 
-        # Check if the current time is beyond the deletion deadline
+        # Check if the current time is beyond the edit deadline
         if timezone.now() > check_in_deadline:
             messages.error(
                 self.request,
-                'Bookings cannot be deleted within 24 hours before check-in. \
+                'Bookings can only be edited up to 24 hours before check-in. \
                     Please contact us for assistance.'
             )
             return HttpResponseRedirect(self.success_url)
