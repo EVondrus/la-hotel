6749486a4350e0da06b2ee9a5e0543b29bd277f1
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-08 15:37:47 +0200

    feat: Add booking list, edit booking, and delete booking url patterns

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import AddBooking
+from .views import(
+    AddBooking, 
+    BookingList, 
+    EditBooking, 
+    DeleteBooking
+    #,BookingSuccess
+)
 
 app_name = 'booking'
 
 urlpatterns = [
-    path('', AddBooking.as_view(), name='add_booking')
+    path('', AddBooking.as_view(), name='add_booking'),
+    #path('success/', BookingSuccess.as_view(), name='booking_success'),
+    path('list/', BookingList.as_view(), name='booking_list'),
+    path('edit/<int:pk>', EditBooking.as_view(), name='edit_booking'),
+    path('delete/<int:pk>/', DeleteBooking.as_view(), name='delete_booking'),
 ]
