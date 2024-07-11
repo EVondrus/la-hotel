82fb626e1a3446b4c82bf56cd3a1befb5e6733a9
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-11 16:58:40 +0200

    feat: Add get_object method to retrieve Profile for updating and format code
    retrieve the Profile object based on the user's primary key (pk) from the URL and  ensure that the correct Profile object is fetched for the logged-in user allowing to update
    
    modify the code to fit PEP8 standards for better readability and maintainability and enhance existing code with comprehensive docstrings for improved documentation

--- guests/views.py
+++ guests/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView, UpdateView, DeleteView, CreateView
+from django.views.generic import (
+    TemplateView,
+    UpdateView,
+    DeleteView
+)
 from allauth.account.views import SignupView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
@@ -6,7 +10,7 @@ from django.contrib.auth import logout
 
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 from .models import Profile
 from .forms import ProfileForm
@@ -15,21 +19,36 @@ from .forms import CustomSignupForm
 
 # Create your views here.
 class CustomSignup(SignupView):
-    """ Custom Signup view """
+    """ Custom Signup view that handles user registration. """
     template_name = 'allauth/account/signup.html'
     form_class = CustomSignupForm
     success_url = reverse_lazy('account_login')
 
     def form_valid(self, form):
-        """Handle successful form submission."""
+        """
+        Handle successful form submission.
+
+        Args:
+            form (CustomSignupForm): The valid form instance.
+
+        Returns:
+            HttpResponse: The response after form is validated.
+        """
         messages.success(
             self.request, 'Your account has been created. You can now log in.'
         )
         return super().form_valid(form)
 
-
     def form_invalid(self, form):
-        """Handle invalid form submission."""
+        """
+        Handle invalid form submission.
+
+        Args:
+            form (CustomSignupForm): The invalid form instance.
+
+        Returns:
+            HttpResponse: The response after form is invalid.
+        """
         messages.error(
             self.request,
             'There was an error with your submission. Please try again.'
@@ -37,15 +56,22 @@ class CustomSignup(SignupView):
         return super().form_invalid(form)
 
 
-
 class Profiles(TemplateView):
-    """  
+    """
     Displays guest profile information
     """
     template_name = 'guests/profiles.html'
-    
+
     def get_context_data(self, **kwargs):
-        """Get the guest profile information."""
+        """
+        Get the profile information.
+
+        Args:
+            **kwargs: Additional context data.
+
+        Returns:
+            dict: The context data including profile and form.
+        """
         user_id = self.kwargs.get('pk')
         profile = get_object_or_404(Profile, user=user_id)
         context = {
@@ -57,18 +83,48 @@ class Profiles(TemplateView):
 
 
 class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    """ Edit Guest Profile """
+    """ Edit user profile information """
     model = Profile
     form_class = ProfileForm
 
     def form_valid(self, form):
-        """Handle successful form submission."""
+        """
+        Handle successful form submission.
+
+        Args:
+            form (ProfileForm): The valid form instance.
+
+        Returns:
+            HttpResponse: The response after form is validated.
+        """
         self.success_url = f'/profiles/profile/{self.kwargs["pk"]}/'
-        messages.success(self.request, 'Your profile was successfully updated.')
+        messages.success(
+            self.request, 'Your profile was successfully updated.'
+        )
         return super().form_valid(form)
 
+    def get_object(self):
+        """
+        Retrieve the Profile object based on the user
+        primary key (pk) from the URL.
+
+        Returns:
+            Profile: The Profile object associated
+            with the given user primary key.
+        
+        Raises:
+            Profile.DoesNotExist: If no Profile is found
+            for the given user primary key.
+    """
+        return Profile.objects.get(user__pk=self.kwargs["pk"])
+
     def test_func(self):
-        """Ensure user can only edit their own profile."""
+        """
+        Test if the user is authorized to edit the profile.
+
+        Returns:
+            bool: True if the user is authorized, otherwise False.
+        """
         return self.request.user == self.get_object().user
 
 
@@ -79,23 +135,32 @@ class DeleteProfile(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """
     model = Profile
     template_name = 'allauth/account/delete_profile.html'
-    success_url = reverse_lazy('home') 
+    success_url = reverse_lazy('home')
 
     def test_func(self):
-        """Ensure user can only delete their own profile."""
+        """
+        Ensure user can only delete their own profile.
+
+        Returns:
+            bool: True if the user is authorized, otherwise False.
+        """
         return self.request.user == self.get_object().user
 
-
     def form_valid(self, form):
-        """Handle successful deletion."""
+        """
+        Handle successful deletion.
+
+        Args:
+            form (ModelForm): The form instance.
+
+        Returns:
+            HttpResponseRedirect: The response redirecting to home.
+        """
         messages.success(self.request, 'Your account has been permanently \
         deleted, including all related bookings. Welcome back anytime!')
 
-        # Delete the user's account and related bookings
         self.request.user.delete()
 
-        # Log out the user
         logout(self.request)
 
-        # Redirect to the home page 
         return HttpResponseRedirect(reverse('home'))
