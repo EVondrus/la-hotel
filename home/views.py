f82d8e46cbea1218af9e6b031b7d6e7797e26b9f
Ellenor Vondrus <ellavondrus@gmail.com>
2024-07-12 11:57:05 +0200

    feat: Add about page template and view and url

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home_page(request):
     return render(request, 'home/index.html')
+
+
+def about_page(request):
+    return render(request, 'home/about.html')
